8815992ae284339cfc2934896dab37bd2a4004ad
Mash J Prime <mashjprime@gmail.com>
2019-11-17 14:43:17 +0800

    Working android app

--- Phoney/MainActivity.cs
+++ Phoney/MainActivity.cs
@@ -15,6 +15,26 @@ namespace Phoney
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            TextView translatedPhoney = FindViewById<TextView>(Resource.Id.TranslatedPhoney);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+
+            // Add code to translate number
+            translateButton.Click += (sender, e) =>
+            {
+                // Translate user's alphanumeric phone number to numeric
+                string translatedNumber = Core.PhoneyTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                {
+                    translatedPhoney.Text = string.Empty;
+                }
+                else
+                {
+                    translatedPhoney.Text = translatedNumber;
+                }
+            };
+
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
